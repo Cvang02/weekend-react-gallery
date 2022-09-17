bdf09d93175702105eb04cbc873401a9b270a03a
Chameng Vang <Chameng.vang@outlook.com>
2022-09-17 14:40:29 -0500

    PUT route is working and likes button is working as intended.

--- src/components/App/GalleryItem/GalleryItem.jsx
+++ src/components/App/GalleryItem/GalleryItem.jsx
@@ -1,10 +1,26 @@
+import axios from 'axios'
 
 function GalleryItem ({galleryItem, fetchGallery}) {
+
+    const galleryDescription = () => {
+        axios({
+            method: 'PUT',
+            url: `/gallery/like/${galleryItem.id}`
+        })
+        .then((putRes) => {
+            console.log('PUT Route Successful', putRes);
+            fetchGallery();
+        })
+        .catch((putErr) => {
+            console.log('PUT Route Unsuccessful', putErr);
+        })
+    }
+
     return (
         <div>
             <img src="images/goat_small.jpg"/>
-            <p>Desciption: {galleryItem.decription}</p>
-            <p>Likes: {galleryItem.likes}</p>
+            <p>Desciption: {galleryItem.description}</p>
+            <button onClick={galleryDescription}>❤️</button> = {galleryItem.likes}
         </div>
     )
 }
